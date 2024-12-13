6e3309a56e1ea5a12b34f58a926ff2b9ec0881fc
jellyyelly <seojs0511@naver.com>
2024-12-13 23:02:20 +0900

    feat: CORS 설정

--- src/main/java/bsise/server/config/SecurityConfigDev.java
+++ src/main/java/bsise/server/config/SecurityConfigDev.java
@@ -1,13 +1,19 @@
 package bsise.server.config;
 
+import static bsise.server.auth.jwt.JwtConstant.X_REFRESH_TOKEN;
+
+import java.util.Arrays;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity(debug = true)
 @Configuration
@@ -23,12 +29,26 @@ public class SecurityConfigDev {
         http.csrf(AbstractHttpConfigurer::disable);
 
         // cors
-        http.cors(AbstractHttpConfigurer::disable);
+        http.cors(cors -> cors.configurationSource(source -> corsConfiguration()));
 
         // url pattern
         http.authorizeHttpRequests(requests -> requests
                 .anyRequest().permitAll())
         ;
         return http.build();
+    }
+
+    @Bean
+    public CorsConfiguration corsConfiguration() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        corsConfig.setAllowedMethods(Arrays.asList("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"));
+        corsConfig.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setExposedHeaders(
+                Arrays.asList(HttpHeaders.AUTHORIZATION, X_REFRESH_TOKEN, "Cache-Control", "Content-Type"));
+        corsConfig.setMaxAge(3600L);
+
+        return corsConfig;
     }
 }
