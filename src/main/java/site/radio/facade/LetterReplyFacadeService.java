0f243f020ff5168f007f78bd76e5330eea02eea6
jellyyelly <seojs0511@naver.com>
2025-01-17 18:39:41 +0900

    refactor: 처리율 제한기 롤백 시 이벤트를 사용하지 않도록 변경

--- src/main/java/site/radio/facade/LetterReplyFacadeService.java
+++ src/main/java/site/radio/facade/LetterReplyFacadeService.java
@@ -4,7 +4,6 @@ import feign.FeignException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.radio.clova.dto.ClovaResponseDto;
@@ -14,7 +13,6 @@ import site.radio.error.RateLimitException;
 import site.radio.letter.Letter;
 import site.radio.letter.LetterRequestDto;
 import site.radio.letter.LetterService;
-import site.radio.limiter.RateLimitRollbackEvent;
 import site.radio.limiter.RateLimitService;
 import site.radio.reply.ReplyResponseDto;
 import site.radio.reply.ReplyService;
@@ -28,7 +26,6 @@ public class LetterReplyFacadeService {
     private final ClovaService clovaService;
     private final LetterService letterService;
     private final ReplyService replyService;
-    private final ApplicationEventPublisher eventPublisher;
 
     public TwoTypeMessage sendLetterToClova(LetterRequestDto letterRequestDto) {
         // 사용 횟수 선차감
@@ -43,7 +40,7 @@ public class LetterReplyFacadeService {
         } catch (FeignException e) {
             // 외부 API 호출에 예외 발생 시 사용 횟수 롤백 이벤트 발행, event status => ROLLBACK_REQUIRED
             log.error("clova studio api 호출에 문제가 발생했습니다. userId: {}", letterRequestDto.getUserId());
-            eventPublisher.publishEvent(RateLimitRollbackEvent.createEvent(letterRequestDto.getUserId()));
+            rateLimitService.rollback(letterRequestDto.getUserId());
             throw e;
         }
     }
