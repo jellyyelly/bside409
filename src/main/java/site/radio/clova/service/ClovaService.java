b8734f1f5a2a045329c76f9adb6ccfb7c3f98c33
jellyyelly <seojs0511@naver.com>
2025-02-07 20:44:57 +0900

    feat: HTTP 비동기 요청 메서드 추가 (@Async 적용)

--- src/main/java/site/radio/clova/service/ClovaService.java
+++ src/main/java/site/radio/clova/service/ClovaService.java
@@ -1,13 +1,19 @@
 package site.radio.clova.service;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import site.radio.clova.client.ClovaFeignClient;
 import site.radio.clova.dto.CreateRequest;
 import site.radio.clova.dto.CreateResponse;
 import site.radio.clova.prompt.PromptTemplate;
 
+@Slf4j
 @Profile({"prod", "dev"})
 @Service
 @RequiredArgsConstructor
@@ -17,5 +23,16 @@ public class ClovaService {
 
     public CreateResponse sendWithPromptTemplate(PromptTemplate promptTemplate, String userMessage) {
         return client.sendToClova(CreateRequest.of(promptTemplate, userMessage));
+    }
+
+    @Async("httpRequestExecutor")
+    public CompletableFuture<CreateResponse> sendAsyncWithPromptTemplate(PromptTemplate promptTemplate,
+                                                                         String userMessage) {
+        return CompletableFuture.completedFuture(client.sendToClova(CreateRequest.of(promptTemplate, userMessage)))
+                .exceptionally(t -> {
+                    log.error("외부 API 호출 중 예외 발생", t);
+                    throw new CompletionException(t);
+                })
+                .orTimeout(20, TimeUnit.SECONDS);
     }
 }
